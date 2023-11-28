765d237c77f7708661a65369f467b5a7cb2201ba
shiro-sata <125814898+shiro-sata@users.noreply.github.com>
2023-11-28 22:37:47 +0100

    camera updates

---
+++ Assets/script/Advertisement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Advertisement : MonoBehaviour {
+
+    public GameObject Image;
+
+	// Use this for initialization
+	void Start () {
+
+        StartCoroutine(Update());
+        SceneManager.UnloadSceneAsync("Office");
+
+    }
+	
+	// Update is called once per frame
+	IEnumerator Update () {
+
+        Resources.UnloadUnusedAssets();
+
+        yield return new WaitForSeconds(10f);
+
+        Image.GetComponent<Animator>().enabled = true;
+
+        yield return new WaitForSeconds(2f);
+
+        SceneManager.LoadScene("Office");
+	}
+}
