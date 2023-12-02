04a1109502630581ce6acd87ed7579f279dc860d
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2023-12-02 16:38:27 +0100

    main menu and move with keyboard on the main menu

---
+++ Assets/Scripts/UI/MainMenuNavigation.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuNavigation : MonoBehaviour
+{
+
+    public Button[] MainMenuButtons;
+    public Text[] MainMenuSelectionTexts;
+
+    private int selectedIndex = 0;
+
+    void Start()
+    {
+        UpdateSelectionTexts();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            selectedIndex = (selectedIndex - 1 + MainMenuButtons.Length) % MainMenuButtons.Length;
+            UpdateSelectionTexts();
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            selectedIndex = (selectedIndex + 1) % MainMenuButtons.Length;
+            UpdateSelectionTexts();
+        }
+    }
+
+    void UpdateSelectionTexts()
+    {
+        for (int i = 0; i < MainMenuButtons.Length; i++)
+        {
+            MainMenuSelectionTexts[i].gameObject.SetActive(i == selectedIndex);
+        }
+    }
+}
