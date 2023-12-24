b5f9f690b3eafc0af4aec2e112a197003a76e248
shiro-sata <125814898+shiro-sata@users.noreply.github.com>
2023-12-24 02:31:40 +0100

    GameOver added

---
+++ Assets/Scripts/GameOverWait.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverWait : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(InitCoroutine());
+		
+	}
+	
+	// Update is called once per frame
+
+	IEnumerator InitCoroutine()
+	{
+		//hi, it's shiro-sata. if you read this, it mean you're gay
+		yield return new WaitForSeconds(21);
+		SceneManager.LoadScene("MainMenu");
+
+	}
+}
