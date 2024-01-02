efcfef84a2d06991062ea983a4fa8fb0f5f50788
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2024-01-02 15:14:23 +0100

    created a debug scene

---
+++ Assets/Scripts/DebugFunctions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class DebugFunctions : MonoBehaviour
+{
+    private float NightNumber;
+
+    void Start()
+    {
+        NightNumber = PlayerPrefs.GetFloat("NightNumber", 1);
+    }
+
+    public void NightAdd()
+    {
+        NightNumber++;
+        PlayerPrefs.SetFloat("NightNumber", NightNumber);
+        PlayerPrefs.Save();
+        Debug.Log("Night number = " + NightNumber);
+    }
+}
