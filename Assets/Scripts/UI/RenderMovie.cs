1c92b9f9f656d465847e35a2bfc660dbee3dac3b
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2024-01-06 23:56:05 +0100

    tried a script for the video player

---
+++ Assets/Scripts/UI/RenderMovie.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RenderMovie : MonoBehaviour
+{
+    public MovieTexture movTexture;
+
+    void Start()
+    {
+        GetComponent<MeshRenderer>().material.mainTexture = movTexture;
+
+        movTexture.Play();
+    }
+}
