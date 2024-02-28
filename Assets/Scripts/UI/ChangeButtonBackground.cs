c5e383789610a60e5d4913b92bab7bf6908d7577
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2024-02-28 19:52:34 +0100

    added background image for buttons on minimap

---
+++ Assets/Scripts/UI/ChangeButtonBackground.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeButtonBackground : MonoBehaviour {
+
+    public Sprite defaultBackgroundSprite;
+    public Sprite greenBackgroundSprite;
+    public Image buttonSourceImage;
+    public Image[] otherButtons;
+
+    public void ButtonPressed() {
+        buttonSourceImage.sprite = greenBackgroundSprite;
+        foreach (Image button in otherButtons)
+        {
+            button.sprite = defaultBackgroundSprite;
+        }
+    }
+}
