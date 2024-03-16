10f88d4ed9077ab3d1790582dcb564139d8d5806
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2024-03-16 19:22:27 +0100

    check version number from an other source

--- Assets/Scripts/UI/VersionCheck.cs
+++ Assets/Scripts/UI/VersionCheck.cs
@@ -5,14 +5,22 @@ public class VersionCheck : MonoBehaviour
 {
     public GameObject objectToActivate;
 
+    [System.Serializable]
+    public class VersionData
+    {
+        public string latest;
+    }
+
     private void Start()
     {
+        objectToActivate.SetActive(false);
+
         StartCoroutine(CheckVersion());
     }
 
     IEnumerator CheckVersion()
     {
-        string url = "https://api.portal-wiiu-edition.com/version.txt";
+        string url = "https://itch.io/api/1/x/wharf/latest?game_id=2435336&channel_name=wup";
 
         using (WWW www = new WWW(url))
         {
@@ -20,7 +28,8 @@ public class VersionCheck : MonoBehaviour
 
             if (string.IsNullOrEmpty(www.error))
             {
-                string onlineVersion = www.text;
+                VersionData data = JsonUtility.FromJson<VersionData>(www.text);
+                string onlineVersion = data.latest;
 
                 TextAsset localVersionAsset = Resources.Load<TextAsset>("Meta/version");
                 string localVersion = localVersionAsset.text;
