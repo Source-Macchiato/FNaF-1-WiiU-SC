e12362b89b916d0226bd775afe62062bbb9bddac
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2024-06-24 23:01:00 +0200

    started ShareData script

---
+++ Assets/Scripts/OnlineSystem/ShareData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ShareData : MonoBehaviour
+{
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+	private void GetData()
+	{
+		// Get game version
+        TextAsset versionAsset = Resources.Load<TextAsset>("Meta/version");
+        string localVersion = versionAsset.text;
+    }
+}
