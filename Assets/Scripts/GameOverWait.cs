f327b225e07a6893cc4cc030b9bfcd1c48fcc8ec
Xyla-TV <namilabest@gmail.com>
2024-09-30 16:56:37 +0200

    fixed all scenes UI

--- Assets/Scripts/GameOverWait.cs
+++ Assets/Scripts/GameOverWait.cs
@@ -2,22 +2,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameOverWait : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		StartCoroutine(InitCoroutine());
-		
+public class GameOverWait : MonoBehaviour
+{
+	void Start()
+	{
+		StartCoroutine(InitCoroutine());	
 	}
-	
-	// Update is called once per frame
 
 	IEnumerator InitCoroutine()
 	{
 		//hi, it's shiro-sata. if you read this, it mean you're gay
-		// hi, it's alyx and I want to say that shiro is gay
+		// hi, it's alyx and I want to say that shiro is gay (1 year later it's still true)
 		yield return new WaitForSeconds(21);
 		SceneManager.LoadScene("MainMenu");
-
 	}
 }
