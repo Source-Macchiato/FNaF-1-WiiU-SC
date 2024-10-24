123ed6d8c405a338d94c49e1cbe3c79884672769
shiro-sata <125814898+shiro-sata@users.noreply.github.com>
2024-10-24 18:37:00 +0200

    Freddy PowerOut jumpscare updated

--- Assets/Scripts/Power.cs
+++ Assets/Scripts/Power.cs
@@ -5,18 +5,27 @@ public class Power : MonoBehaviour {
 
     public GameObject FreddySongDarkOffice;
     public GameObject freddyJumpScare;
+    public GameObject Blink;
     public GameObject Song;
+    public GameObject BlinkAudio;
 
     //---FLOATS---------------------------
     public float WaitBeforeStart = 20f;
-    public float PlayTime = 15f;
+    public float PlayTime; // Removed the fixed value to assign it randomly
+    public float DarkTime; // random
     public float JumpscarePlayTime = 0.2f;
 
     private ControllersRumble controllersRumble;
 
     void Start()
     {
+        Blink.SetActive(false);
         controllersRumble = FindObjectOfType<ControllersRumble>();
+
+        // Assign PlayTime a random value between 15 and 28 seconds
+        PlayTime = Random.Range(15f, 28f);
+        //Asign a random value to draktime
+        DarkTime = Random.Range(5f, 10f);
     }
 
     void Update()
@@ -32,16 +41,23 @@ public class Power : MonoBehaviour {
 
             if (PlayTime <= 0)
             {
+                //active Blink Image and start countdown and end the song
+                Blink.SetActive(true);
+                DarkTime -= Time.deltaTime;
+                Song.SetActive(false);
+                BlinkAudio.SetActive(true);
+
+                //disable freddy animation
                 FreddySongDarkOffice.GetComponent<Animator>().enabled = false;
-                FreddySongDarkOffice.SetActive(false);
 
-                freddyJumpScare.SetActive(true);
-                PlayTime = 0;
+                if (DarkTime <= 0)
+                {
+                    freddyJumpScare.SetActive(true);
+                    PlayTime = 0;
+                    JumpscarePlayTime -= Time.deltaTime;
 
-                JumpscarePlayTime -= Time.deltaTime;
-
-                controllersRumble.IsRumbleTriggered("No power");
-
+                    controllersRumble.IsRumbleTriggered("No power");
+                }
                 if (JumpscarePlayTime <= 0)
                 {
                     SceneManager.LoadScene("GameOver");
