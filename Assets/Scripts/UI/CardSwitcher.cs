8e28f7302b935c7d9ce8c8dcfa70df87dda42226
Xyla-TV <namilabest@gmail.com>
2024-10-27 16:26:54 +0100

    card switcher UI and script

---
+++ Assets/Scripts/UI/CardSwitcher.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using RTLTMPro;
+
+public class CardSwitcher : MonoBehaviour
+{
+	public int difficultyId;
+
+	public int minValue;
+	public int maxValue;
+
+	public RTLTextMeshPro tmpSwitcherValue;
+
+	void Start()
+	{
+		UpdateTextValue();
+	}
+
+	public void IncreaseDifficulty()
+	{
+		if (difficultyId >= minValue && difficultyId < maxValue)
+		{
+            difficultyId++;
+
+			UpdateTextValue();
+        }
+	}
+
+	public void DecreaseDifficulty()
+	{
+		if (difficultyId > minValue && difficultyId <= maxValue)
+		{
+			difficultyId--;
+
+			UpdateTextValue();
+		}
+	}
+
+	private void UpdateTextValue()
+	{
+        if (tmpSwitcherValue.text != difficultyId.ToString())
+        {
+            tmpSwitcherValue.text = difficultyId.ToString();
+        }
+    }
+}
