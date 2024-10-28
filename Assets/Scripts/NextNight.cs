c5222ec09757ae75104711a5af0cd7f9b7aa04ec
Xyla-TV <namilabest@gmail.com>
2024-10-28 12:07:28 +0100

    display night 7

--- Assets/Scripts/NextNight.cs
+++ Assets/Scripts/NextNight.cs
@@ -1,31 +1,36 @@
 ﻿using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class NextNight : MonoBehaviour {
-
-    public GameObject LoadingSprite;
-
+public class NextNight : MonoBehaviour
+{
+    [Header("Nights")]
     public GameObject Night1;
     public GameObject Night2;
     public GameObject Night3;
     public GameObject Night4;
     public GameObject Night5;
     public GameObject Night6;
+    public GameObject Night7;
+
+    [Header("Loading")]
     private float nightNumber;
-    public Text NightNumberDisplayer;
+    public GameObject LoadingSprite;
     public GameObject loadingScreenPanel;
 
-    void Start () {
-        loadingScreenPanel.SetActive(false);
+    void Start()
+    {
         nightNumber = SaveManager.LoadNightNumber();
+
+        loadingScreenPanel.SetActive(false);
         LoadingSprite.SetActive(false);
+
         Night1.SetActive(false);
         Night2.SetActive(false);
         Night3.SetActive(false);
         Night4.SetActive(false);
         Night5.SetActive(false);
         Night6.SetActive(false);
+        Night7.SetActive(false);
     
         if (nightNumber == 0)
         {
@@ -51,11 +56,16 @@ public class NextNight : MonoBehaviour {
         {
             Night6.SetActive(true);
         }
+        else if (nightNumber == 6)
+        {
+            Night7.SetActive(true);
+        }
 
         StartCoroutine(InitCoroutine());
     }
 
-    IEnumerator InitCoroutine() {
+    IEnumerator InitCoroutine()
+    {
         yield return new WaitForSeconds(5);
         loadingScreenPanel.SetActive(true);
         LoadingSprite.SetActive(true);
