0e769dceb12629160f639d0980cb829aa2352d92
Xyla-TV <namilabest@gmail.com>
2024-10-30 16:00:11 +0100

    added new achievement

---
+++ Assets/Scripts/WitheredBonnie.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class WitheredBonnie : MonoBehaviour
+{
+	public Sprite achievementIcon;
+
+	void Start()
+	{
+        MedalsManager.medalsManager.ShowAchievement("Withered Bonnie Gaming", "Find the easter egg.", achievementIcon);
+    }
+}
