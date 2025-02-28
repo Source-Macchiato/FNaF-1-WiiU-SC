6e31523550c05d08cc54688776f2d90625c0be13
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2025-02-28 20:58:40 +0100

    called wrong light function

--- Assets/Scripts/Office.cs
+++ Assets/Scripts/Office.cs
@@ -310,7 +310,7 @@ public class Office : MonoBehaviour {
                             }
                             else
                             {
-                                RightLightSystem();
+                                ToggleRightLight();
                             }
                         }
                         else
