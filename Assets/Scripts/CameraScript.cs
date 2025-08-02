22c9988e8ff0c37d7991828d701bad4ea53c8725
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2025-08-02 12:55:57 +0200

    Fixed minimap displayed by default

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -48,6 +48,8 @@ public class CameraScript : MonoBehaviour
         layoutManager = FindObjectOfType<LayoutManager>();
 
         hideOfficeGameObject.SetActive(false);
+        camIsUp = false;
+        minimapGameObject.SetActive(false);
     }
 
     void Update()
