1eb13c5d5da004d6b73317344670e54821226df6
SpanishFreddy <121837347+SpanishFreddy@users.noreply.github.com>
2025-08-02 13:46:50 +0200

    Added finally a working Panoramic Effect
    Monitor UI needs to be moved.

---
+++ Assets/Scripts/Panorama/PanoramicEffect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class PanoramicEffect : MonoBehaviour
+{
+    [Header("Shader Material")]
+    public Material panoramicMaterial;
+
+    [Header("Effect Controls")]
+    public bool enableEffect = true;
+
+    [Range(0f, 1f)]
+    public float curveAmount = 0.3f;
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (enableEffect && panoramicMaterial != null)
+        {
+            panoramicMaterial.SetFloat("_CurveAmount", curveAmount);
+            Graphics.Blit(src, dest, panoramicMaterial);
+        }
+        else
+        {
+            Graphics.Blit(src, dest);
+        }
+    }
+}
